1132601be7c9cc0ed2de301609667e0ce4b1ab03
Marie-LKD <mlkd.298@gmail.com>
2022-07-02 12:39:39 -0700

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*main - entry block
+*Description: print numbers from 0 to 9
+*Return: 0
+*/
+int main(void)
+{
+char c = '0';
+while(c <= '9')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
