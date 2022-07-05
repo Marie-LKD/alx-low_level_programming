bf23e6c6b61fbd5253961095443bc68858d99d79
Marie-LKD <mlkd.298@gmail.com>
2022-07-05 12:58:28 -0700

    functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main - prints putchar
+*
+*Return: 0 Success
+*/
+int main(void)
+{
+char word[8] = "_putchar";
+int i;
+for (i = 0; i <= 7; i++)
+{
+putchar(word[i]);
+}
+putchar('\n');
+return(0);
+}
