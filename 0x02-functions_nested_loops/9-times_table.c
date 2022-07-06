fd770531d26352d1411ec4e413223b82deef0474
Marie-LKD <mlkd.298@gmail.com>
2022-07-06 13:27:34 -0700

    functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,7 +22,7 @@ if (result >= 10)
 _putchar((result / 10) + '0');
 _putchar((result % 10) + '0');
 }
-else if ((result < 10) && (b != 10))
+else if ((result < 10) && (b != 0))
 {
 _putchar(' ');
 _putchar((result % 10) + '0');
