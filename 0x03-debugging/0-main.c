1ccce264cc2261e080080785559bf3ad3bd23a56
Marie-LKD <mlkd.298@gmail.com>
2022-07-08 13:45:57 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
-*main - tests function that prints if integer is
+*main - entry block
+*Description: tests function that prints if integer is
 *positive or negative
-*Return: 0
+*Return: 0 Success
 */
 int main(void)
 {
