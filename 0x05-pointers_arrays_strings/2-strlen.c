b482ac08e9760c1cdaf9daf343208b9a073b1556
Marie-LKD <mlkd.298@gmail.com>
2022-07-11 14:40:06 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_strlen - function returns the kength of a string
+*@s: string
+*Return: integer length of a string
+*/
+int _strlen(char *s)
+{
+int length;
+length = 0;
+while (*s != '\0')
+{
+s++;
+lenght++;
+}
+return (length);
+}
